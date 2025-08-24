8566a509ac9e955292c337bb406dd8849d37712c
Angel David Parra <angdavdeveloper@gmail.com>
2025-08-24 15:30:15 +0200

    Updates

---
+++ assets/main-DjSsPs4N.js
@@ -0,0 +1,1 @@
+import"./main-DURRbq0w.js";const t=document.querySelector(".active a");t.addEventListener("click",e=>{e.preventDefault()});
--- assets/main-EGzn_uUG.js
+++
@@ -1,1 +0,0 @@
-import"./main-BBr33Gxp.js";const t=document.querySelector(".active a");t.addEventListener("click",e=>{e.preventDefault()});
